0d753297a88a3335fb0d739daf972912c1deae73
DevShivam <elite.shivam23@gmail.com>
2020-02-16 23:48:10 +0530

    create manage frame

--- Project.py
+++ Project.py
@@ -1,1 +1,66 @@
-print("This is Mywork")
+from tkinter import *
+from tkinter import ttk
+class Student:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Student Management System")
+        self.root.geometry("1920x1080+0+0")
+        
+        title=Label(self.root,text="Student Management System",font=("times new roman",40,"bold"),bg="Yellow",fg="red")
+        title.pack(side=TOP,fill=X)
+
+        #Managing_Frame
+        Manage_Frame=Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
+        Manage_Frame.place(x=20,y=100,width=450,height=600)
+
+        m_title=Label(Manage_Frame,text="Manage Students",bg="crimson",fg="White",font=("times new roman",20,"bold"))
+        m_title.grid(row=0,columnspan=2,pady=20)
+        
+        lbl_roll=Label(Manage_Frame,text="Roll No",bg="crimson",fg="White",font=("times new roman",20,"bold"))
+        lbl_roll.grid(row=1,column=0,pady=10,padx=20,sticky="w")
+
+        txt_Roll=Entry(Manage_Frame,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+        txt_Roll.grid(row=1,column=1,pady=10,padx=20,sticky="w")
+
+        lbl_name=Label(Manage_Frame,text="Name",bg="crimson",fg="White",font=("times new roman",20,"bold"))
+        lbl_name.grid(row=2,column=0,pady=10,padx=20,sticky="w")
+
+        txt_name=Entry(Manage_Frame,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+        txt_name.grid(row=2,column=1,pady=10,padx=20,sticky="w")
+
+        lbl_Email=Label(Manage_Frame,text="Email",bg="crimson",fg="White",font=("times new roman",20,"bold"))
+        lbl_Email.grid(row=3,column=0,pady=10,padx=20,sticky="w")
+
+        txt_Email=Entry(Manage_Frame,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+        txt_Email.grid(row=3,column=1,pady=10,padx=20,sticky="w")
+        
+        lbl_Gender=Label(Manage_Frame,text="Gender",bg="crimson",fg="White",font=("times new roman",20,"bold"))
+        lbl_Gender.grid(row=4,column=0,pady=10,padx=20,sticky="w")
+
+        combo_gender=ttk.Combobox(Manage_Frame,font=("times new roman",14,"bold"),state='readonly')
+        combo_gender['values']=("Male","Female","Others")
+        combo_gender.grid(row=4,column=1,pady=10,padx=20)
+
+        lbl_Contact=Label(Manage_Frame,text="Contact",bg="crimson",fg="White",font=("times new roman",20,"bold"))
+        lbl_Contact.grid(row=5,column=0,pady=10,padx=20,sticky="w")
+
+        txt_Contact=Entry(Manage_Frame,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+        txt_Contact.grid(row=5,column=1,pady=10,padx=20,sticky="w")
+
+        lbl_Dob=Label(Manage_Frame,text="D.O.B",bg="crimson",fg="White",font=("times new roman",20,"bold"))
+        lbl_Dob.grid(row=6,column=0,pady=10,padx=20,sticky="w")
+
+        txt_Dob=Entry(Manage_Frame,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+        txt_Dob.grid(row=6,column=1,pady=10,padx=20,sticky="w")
+
+        lbl_Address=Label(Manage_Frame,text="Address",bg="crimson",fg="White",font=("times new roman",20,"bold"))
+        lbl_Address.grid(row=7,column=0,pady=10,padx=20,sticky="w")
+
+        txt_Address=Text(Manage_Frame,width=25,height=4)
+        txt_Address.grid(row=7,column=1,pady=10,padx=20,sticky="w")
+        #Detail_Frame
+        Detail_Frame=Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
+        Detail_Frame.place(x=500,y=100,width=800,height=560)
+root=Tk()
+ob=Student(root)
+root.mainloop()
